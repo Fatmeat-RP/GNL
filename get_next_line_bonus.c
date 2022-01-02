de23f6b0f94cf592327246f4932fc35dbb716f81
fatmeat <pathofyggdrasil@gmail.com>
2022-01-02 03:15:45 +0100

    added bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,74 @@
+#include "get_next_line.h"
+
+char	*ft_cleanlione(char *line)
+{
+	char	*tmp;
+	char	*p;
+	t_s64	k;
+
+	p = ft_strchr(line, '\n');
+	k = ft_strlen(p);
+	tmp = ft_strndup((p + 1), k);
+	free (line);
+	line = ft_strndup(tmp, k);
+	free (tmp);
+	return (line);
+}
+
+char	*ft_strrchr(char *s, int c)
+{
+	char	*p;
+	char	*tmp;
+
+	p = s + ft_strlen(s);
+	tmp = s;
+	while ((p >= s) && (p != tmp))
+	{
+		if (*p == (char)c)
+			return (p);
+		p--;
+		tmp++;
+	}
+	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*line[1000];
+	char			buf[BUFFER_SIZE + 1];
+	char			*ret;
+	int			rd;
+	t_s64			n;
+
+	if (fd < 0)
+		return (NULL);
+	rd = read(fd, buf, BUFFER_SIZE);
+	buf[rd] = 0;
+	if (!rd && ft_strrchr(line[fd], '\n') == NULL)
+		return (NULL);
+	line[fd] = ft_strjoin(line[fd], buf, rd);
+	while (ft_eol(line[fd]) < 0 && rd)
+	{
+		rd = read(fd, buf, BUFFER_SIZE);
+		line[fd] = ft_strjoin(line[fd], buf, rd);
+	}
+	n = ft_eol(line[fd]) + 1;
+	ret = ft_strndup(line[fd], n);
+	line[fd] = ft_cleanlione(line[fd]);
+	return (ret);
+}
+
+int	main(void)
+{
+	char	*line;
+	int	fd;
+
+	fd = open("bible.txt", O_RDONLY);
+	while (line = get_next_line(fd))
+	{
+		printf("%s", line);
+		free (line);
+	}
+	close(fd);
+	return (0);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acarle-m <acarle-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/28 01:35:03 by acarle-m          #+#    #+#             */
+/*   Updated: 2021/12/29 03:55:51 by acarle-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strndup(char *buff, t_s64 n)
+{
+	char	*s;
+	t_s64	i;
+
+	i = 0;
+	s = malloc(n + 1);
+	if (!s)
+		return (NULL);
+	while (i < n)
+	{
+		s[i] = buff[i];
+		i++;
+	}
+	s[i] = 0;
+	return (s);
+}
+
+t_s64	ft_strlen(char *s)
+{
+	t_s64	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+t_s64	ft_eol(char *s)
+{
+	t_s64	i;
+
+	if (!s)
+		return (-1);
+	i = 0;
+	while (s[i] != '\n')
+	{
+		if (s[i] == 0)
+			return (-1);
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	char	*p;
+
+	if (!s)
+		return (NULL);
+	p = s;
+	while (*p != (char)c)
+	{
+		if (*p == 0)
+			return (NULL);
+		p++;
+	}
+	return (p);
+}
+
+
+char	*ft_strjoin(char *s1, char *s2, int rd)
+{
+	char	*s;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = ft_strlen(s1);
+	s = malloc(j + rd + 1);
+	if (!s)
+		return (NULL);
+	while (i < j)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	while (i < (j + rd))
+	{
+		s[i] = s2[i - j];
+		i++;
+	}
+	s[i] = 0;
+	if (j)
+		free(s1);
+	return (s);
+}
